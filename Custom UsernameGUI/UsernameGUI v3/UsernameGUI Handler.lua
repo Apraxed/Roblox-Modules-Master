006186e64063fd2361d913593e2c98f6a311a7fa
Z <78899706+Apraxed@users.noreply.github.com>
2022-11-21 23:28:57 -0800

    Update UsernameGUI Handler.lua
    it didn't save that well so I just fixed a few lines

--- Custom UsernameGUI/UsernameGUI v3/UsernameGUI Handler.lua
+++ Custom UsernameGUI/UsernameGUI v3/UsernameGUI Handler.lua
@@ -10,7 +10,7 @@ game:GetService("Players").PlayerAdded:Connect(function(plr)
 	plr.CharacterAdded:Connect(function(char)
 		local clone = require(11470095967).guiv3:Clone()
 		clone.Parent = char.HumanoidRootPart
-    clone.Line1.Text = plr.DisplayName
-    clone.Line2.Text = "@"..plr.Name
-  end)
+		clone.Line1.Text = plr.DisplayName
+		clone.Line2.Text = "@"..plr.Name
+	end)
 end)
