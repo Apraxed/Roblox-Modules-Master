9c31778efa4c63f7ec077f6fe655d285f175d00b
Z <78899706+Apraxed@users.noreply.github.com>
2022-11-21 21:36:44 -0800

    Create UsernameGUI Handler.lua

---
+++ Custom UsernameGUI/UsernameGUI v3/UsernameGUI Handler.lua
@@ -0,0 +1,16 @@
+-- // Copyright 2022, Aprax3d, All rights reserved.
+
+game:GetService("Players").PlayerAdded:Connect(function(plr)
+	plr.CharacterAdded:Connect(function(char)
+		char:WaitForChild("Humanoid").DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+	end)
+end)
+
+game:GetService("Players").PlayerAdded:Connect(function(plr)
+	plr.CharacterAdded:Connect(function(char)
+		local clone = require(11470095967).guiv3:Clone()
+		clone.Parent = char.HumanoidRootPart
+    clone.Line1.Text = plr.DisplayName
+    clone.Line2.Text = plr.Name
+  end)
+end)
