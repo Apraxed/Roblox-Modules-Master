d05cec2ecf36b4aef9b11c5da0c41f1b0950575b
Zack <78899706+Apraxed@users.noreply.github.com>
2023-01-03 23:04:06 -0800

    Readability CustomUsernameHandler v2 and v3

--- Custom UsernameGUI/UsernameGUI v2/Custom UsernameGUI.lua
+++ Custom UsernameGUI/UsernameGUI v2/Custom UsernameGUI.lua
@@ -4,10 +4,9 @@ local Players = game:GetService("Players")
 
 local function OnEvent(plr, char)
 	char:WaitForChild("Humanoid").DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
-	local clone = require(11470095967).guiv3:Clone()
-	clone.Parent = char.HumanoidRootPart
-	clone.Line1.Text = plr.DisplayName
-	clone.Line2.Text = "@"..plr.Name
+	local clone = require(11470095967).guiv2:Clone()
+	clone.Parent = char.Head
+	clone.NameText.Text = plr.DisplayName.." (@"..plr.Name..")"
 end
 
 Players.PlayerAdded:Connect(function(plr)
